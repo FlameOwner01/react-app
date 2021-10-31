7a8b4a009b17a4ffc6e5045e66bed4ff489a208d
FlameOwner <73409497+FlameOwner01@users.noreply.github.com>
2021-10-31 18:38:25 +0100

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import AppFooter from './AppFooter';
+import AppHeader from './AppHeader';
+import Home from './Home';
+import Students from './Students';
+import Courses from './Courses';
+import Course from './Course';
+
+export { AppFooter, AppHeader, Courses, Students, Home, Course }
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './index.css';
+import App from './App.js';
+import { render } from 'react-dom';
+import './assets/styles/site.css';
+import './assets/styles/table.css'
+
+
+render(<App />, document.getElementById('root'));
